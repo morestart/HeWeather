24f2ddd48fb392afab9cc6ead0b5416a4c3c169d
cattree <morestarforyou@gamil.com>
2018-03-14 08:02:36 +0800

    add tmp_max/tmp_min/pop

--- HeWeather.py
+++ HeWeather.py
@@ -7,7 +7,6 @@ from homeassistant.const import ATTR_ATTRIBUTION, ATTR_FRIENDLY_NAME
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import track_time_interval
-import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -21,30 +20,31 @@ CONF_APPKEY = "appkey"
 life_index_list = {"comf_txt": None, "drsg_txt": None, "flu_txt": None,
                    "sport_txt": None, "trav_txt": None, "uv_txt": None, "cw_txt": None}
 
-OPTIONS = {
-    "fl": ["HeWeather_fl", "体感温度", "mdi:temperature-celsius", "℃"],
-    "tmp": ["HeWeather_tmp", "室外温度", "mdi:thermometer", "℃"],
-    "hum": ["HeWeather_hum", "室外湿度", "mdi:water-percent", "%Rh"],
-    "pcpn": ["HeWeather_pcpn", "降水量", "mdi:weather-rainy", "mm"],
-    "pres": ["HeWeather_pres", "大气压", "mdi:debug-step-over", "hPa"],
-    "vis": ["HeWeather_vis", "能见度", "mdi:eye", "km"],
-    "wind_spd": ["HeWeather_wind_spd", "风速", "mdi:speedometer", "km/h"],
-    "wind_sc": ["HeWeather_wind_sc", "风力", "mdi:flag-variant", None],
-    "wind_dir": ["HeWeather_wind_dir", "风向", "mdi:weather-windy", None],
-    "cond_txt": ["HeWeather_cond_txt", "天气状态", "mdi:presentation", None],
-    "qlty": ["HeWeather_qlty", "空气质量", "mdi:beach", None],
-    "main": ["HeWeather_main", "主要污染物", "mdi:chart-bar-stacked", None],
-    "aqi": ["HeWeather_aqi", "空气质量指数", "mdi:poll", "AQI"],
-    "pm10": ["HeWeather_pm10", "PM10", "mdi:blur", "μg/m³"],
-    "pm25": ["HeWeather_pm25", "PM2.5", "mdi:blur", "μg/m³"],
-    "comf": ["HeWeather_comf", "舒适度指数", "mdi:chart-bubble", None],
-    "cw": ["HeWeather_cw", "洗车指数", "mdi:car-wash", None],
-    "drsg": ["HeWeather_drsg", "穿衣指数", "mdi:tie", None],
-    "flu": ["HeWeather_flu", "感冒指数", "mdi:seat-individual-suite", None],
-    "sport": ["HeWeather_sport", "运动指数", "mdi:bike", None],
-    "uv": ["HeWeather_uv", "紫外线指数", "mdi:sunglasses", None],
-    "trav": ["HeWeather_trav", "出行指数", "mdi:bus", None]
-}
+OPTIONS = dict(fl=["HeWeather_fl", "实时体感温度", "mdi:temperature-celsius", "℃"],
+               tmp=["HeWeather_tmp", "实时室外温度", "mdi:thermometer", "℃"],
+               hum=["HeWeather_hum", "实时室外湿度", "mdi:water-percent", "%Rh"],
+               pcpn=["HeWeather_pcpn", "降水量", "mdi:weather-rainy", "mm"],
+               pres=["HeWeather_pres", "大气压", "mdi:debug-step-over", "hPa"],
+               vis=["HeWeather_vis", "能见度", "mdi:eye", "km"],
+               wind_spd=["HeWeather_wind_spd", "风速", "mdi:speedometer", "km/h"],
+               wind_sc=["HeWeather_wind_sc", "风力", "mdi:flag-variant", None],
+               wind_dir=["HeWeather_wind_dir", "风向", "mdi:weather-windy", None],
+               cond_txt=["HeWeather_cond_txt", "天气状态", "mdi:presentation", None],
+               qlty=["HeWeather_qlty", "空气质量", "mdi:beach", None],
+               main=["HeWeather_main", "主要污染物", "mdi:chart-bar-stacked", None],
+               aqi=["HeWeather_aqi", "空气质量指数", "mdi:poll", "AQI"],
+               pm10=["HeWeather_pm10", "PM10", "mdi:blur", "μg/m³"],
+               pm25=["HeWeather_pm25", "PM2.5", "mdi:blur", "μg/m³"],
+               comf=["HeWeather_comf", "舒适度指数", "mdi:chart-bubble", None],
+               cw=["HeWeather_cw", "洗车指数", "mdi:car-wash", None],
+               drsg=["HeWeather_drsg", "穿衣指数", "mdi:tie", None],
+               flu=["HeWeather_flu", "感冒指数", "mdi:seat-individual-suite", None],
+               sport=["HeWeather_sport", "运动指数", "mdi:bike", None],
+               uv=["HeWeather_uv", "紫外线指数", "mdi:sunglasses", None],
+               trav=["HeWeather_trav", "出行指数", "mdi:bus", None],
+               tmp_max=["Heweather_tmp_max", "今日最高温度", "mdi:mdi:thermometer", "℃"],
+               tmp_min=["Heweather_tmp_min", "今日最低温度", "mdi:mdi:thermometer", "℃"],
+               pop=["Heweather_pop", "降水概率", "mdi:weather-rainy", "%"])
 
 ATTR_UPDATE_TIME = "更新时间"
 ATTRIBUTION = "Powered by He Weather"
@@ -63,6 +63,7 @@ class WeatherData(object):
         self._url = "https://free-api.heweather.com/s6/weather/now?parameters"
         self._air_url = "https://free-api.heweather.com/s6/air/now?parameters"
         self._life_index_url = "https://free-api.heweather.com/s6/weather/lifestyle?parameters"
+        self._long_weather_forcasting_url = "https://free-api.heweather.com/s6/weather/forecast?parameters"
         self._params = {"location": city, "key": appkey}
         self._aqi_params = {"location": aqi_city, "key": appkey}
         self._fl = None
@@ -88,8 +89,11 @@ class WeatherData(object):
         self._sport = None
         self._uv = None
         self._trav = None
+        self._tmp_max = None
+        self._tmp_min = None
+        self._pop = None
 
-        self.update(dt_util.now())
+        self.update()
 
         track_time_interval(hass, self.update, TIME_BETWEEN_UPDATES)
 
@@ -182,11 +186,22 @@ class WeatherData(object):
         return self._trav
 
     @property
+    def tmp_max(self):
+        return self._tmp_max
+
+    @property
+    def tmp_min(self):
+        return self._tmp_min
+
+    @property
+    def pop(self):
+        return self._pop
+
+    @property
     def updatetime(self):
         return self._updatetime
 
-
-    def update(self, now):
+    def update(self):
         _LOGGER.info("Update from HeWeather...")
         try:
             r = requests.get(self._url, self._params, verify=True)
@@ -201,8 +216,9 @@ class WeatherData(object):
             self._wind_spd = con["HeWeather6"][0]["now"]["wind_spd"]
             self._wind_sc = con["HeWeather6"][0]["now"]["wind_sc"]
             self._wind_dir = con["HeWeather6"][0]["now"]["wind_dir"]
-        except:
+        except ConnectionError:
             _LOGGER.info("连接失败")
+
         try:
             r_air = requests.get(self._air_url, self._aqi_params, verify=True)
             con_air = r_air.json()
@@ -211,27 +227,35 @@ class WeatherData(object):
             self._aqi = con_air["HeWeather6"][0]["air_now_city"]["aqi"]
             self._pm10 = con_air["HeWeather6"][0]["air_now_city"]["pm10"]
             self._pm25 = con_air["HeWeather6"][0]["air_now_city"]["pm25"]
-        except:
+        except ConnectionError:
             _LOGGER.info("连接失败")
-        
         try:
             life_index = requests.get(self._life_index_url, self._params, verify=True)
             con_life_index = life_index.json()
-            self._comf = con_life_index["HeWeather6"][0]["lifestyle"][0]["brf"]
-            self._drsg = con_life_index["HeWeather6"][0]["lifestyle"][1]["brf"]
-            self._flu = con_life_index["HeWeather6"][0]["lifestyle"][2]["brf"]
-            self._sport = con_life_index["HeWeather6"][0]["lifestyle"][3]["brf"]
-            self._trav = con_life_index["HeWeather6"][0]["lifestyle"][4]["brf"]
-            self._uv = con_life_index["HeWeather6"][0]["lifestyle"][5]["brf"]
-            self._cw = con_life_index["HeWeather6"][0]["lifestyle"][6]["brf"]
+            # self._comf = con_life_index["HeWeather6"][0]["lifestyle"][0]["brf"]
+            # self._drsg = con_life_index["HeWeather6"][0]["lifestyle"][1]["brf"]
+            # self._flu = con_life_index["HeWeather6"][0]["lifestyle"][2]["brf"]
+            # self._sport = con_life_index["HeWeather6"][0]["lifestyle"][3]["brf"]
+            # self._trav = con_life_index["HeWeather6"][0]["lifestyle"][4]["brf"]
+            # self._uv = con_life_index["HeWeather6"][0]["lifestyle"][5]["brf"]
+            # self._cw = con_life_index["HeWeather6"][0]["lifestyle"][6]["brf"]
 
             life = ["comf_txt", "drsg_txt", "flu_txt", "sport_txt", "trav_txt", "uv_txt", "cw_txt"]
 
             for i, index in enumerate(life):
                 life_index_list[index] = con_life_index["HeWeather6"][0]["lifestyle"][i]["txt"]
-
-        except:
+        except ConnectionError:
             _LOGGER.info("连接失败")
+
+        try:
+            r = requests.get(self._long_weather_forcasting_url, self._params, verify=True)
+            today_weather = r.json()
+            self._tmp_max = today_weather["HeWeather6"][0]["daily_forecast"][0]["tmp_max"]
+            self._tmp_min = today_weather["HeWeather6"][0]["daily_forecast"][0]["tmp_min"]
+            self._pop = today_weather["HeWeather6"][0]["daily_forecast"][0]["pop"]
+        except ConnectionError:
+            _LOGGER.info("连接失败")
+
         import time
         self._updatetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
@@ -352,3 +376,9 @@ class HeWeatherSensor(Entity):
             self._state = self._data.trav
         elif self._type == "uv":
             self._state = self._data.uv
+        elif self._type == "tmp_max":
+            self._state = self._data.tmp_max
+        elif self._type == "tmp_min":
+            self._state = self._data.tmp_min
+        elif self._state == "pop":
+            self._state = self._data.pop
